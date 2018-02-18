bbf7dce0d7e61b85631198f01f6730e4d15f0572
Alphzeno Technology Services <alphzeno@gmail.com>
2018-02-18 16:57:05 +0300

    Create DBConfiguration.java

---
+++ DBConfiguration.java
@@ -0,0 +1,82 @@
+package com.az.db.conf;
+
+import java.util.Properties;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+ 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+ 
+@Configuration
+@EnableTransactionManagement
+public class DBConfiguration {
+    @Value("${db.driver}")
+    private String DRIVER;
+ 
+    @Value("${db.password}")
+    private String PASSWORD;
+ 
+    @Value("${db.url}")
+    private String URL;
+ 
+    @Value("${db.username}")
+    private String USERNAME;
+ 
+    @Value("${hibernate.dialect}")
+    private String DIALECT;
+ 
+    @Value("${hibernate.show_sql}")
+    private String SHOW_SQL;
+ 
+    /*@Value("${hibernate.hbm2ddl.auto}")
+    private String HBM2DDL_AUTO;*/
+ 
+    @Value("${entitymanager.packagesToScan}")
+    private String PACKAGES_TO_SCAN;
+    
+    @Value("${jdbc.url}")
+    private String jndiURL;
+ 
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DRIVER);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+ 
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() throws NamingException {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        //sessionFactory.setDataSource(dataSource());
+        sessionFactory.setDataSource(getJNDIDataSource());
+        sessionFactory.setPackagesToScan(PACKAGES_TO_SCAN);
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", DIALECT);
+        hibernateProperties.put("hibernate.show_sql", SHOW_SQL);
+    //    hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
+        sessionFactory.setHibernateProperties(hibernateProperties);
+ 
+        return sessionFactory;
+    }
+ 
+    private DataSource getJNDIDataSource() throws NamingException {
+		return (DataSource) new JndiTemplate().lookup(jndiURL);
+	}
+
+	@Bean
+    public HibernateTransactionManager transactionManager() throws NamingException {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+}
